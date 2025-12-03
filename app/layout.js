deaef34c4423b4dd3806c361b155da149e09d3d9
Humphrey Lionel Gevero <149223212+hamp25@users.noreply.github.com>
2025-12-03 23:10:15 +0800

    add favicon/logo

--- app/layout.js
+++ app/layout.js
@@ -3,9 +3,18 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 export const metadata = {
-  title: 'CoreTek Digital Solutions',
-  description: 'SaaS, e-commerce, software development and IT services.'
-}
+  title: "CoreTek Digital Solutions",
+  description: "SaaS, e-commerce, software development and IT services.",
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/icon.png', sizes: '512x512', type: 'image/png' },
+    ],
+    apple: '/apple-touch-icon.png',
+  }
+};
+
 
 export default function RootLayout({ children }) {
   return (
